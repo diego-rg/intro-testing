f57251b66b4b1263fa865734a254692a7e85718c
eu <diego_thegarden@hotmail.com>
2022-08-25 22:01:09 +0200

    double check

--- util.test.js
+++ util.test.js
@@ -3,7 +3,15 @@
 const { generateText } = require("./util");
 
 //usar valores límite, problemáticos, etc
+//double check (valores límite?), contrario,..
 test("debe devolver un nome e idade", () => {
   const text = generateText("Pepe", 50);
   expect(text).toBe("Pepe (50 years old)");
+  const text2 = generateText("María", 20);
+  expect(text2).toBe("María (20 years old)");
+});
+
+test("debe devolver o texto sin datos", () => {
+  const text = generateText("", null);
+  expect(text).toBe(" (null years old)");
 });
